8147d089c8b8c7259c7ff17353ffe27ec2784985
Rohit Sharma <sharmarohit469494@gmail.com>
2026-05-28 23:24:24 +0530

    Intro to Hashing

---
+++ 034 - Assignment on Hashing/app.js
@@ -0,0 +1,8 @@
+I will add question on Hashing later
+
+There is two kinds of Hashing
+
+setHash -> linear data store krta h array k jaisa, but only unique value
+
+
+mapHash -> key - value pair mein store krta hai
